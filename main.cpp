d1e4145b114b92fd162a8150e604cfcb245b70bd
Stephan Creutz <stephan.cr@gmx.de>
2026-03-24 19:29:52 +0100

    generate some non-sense pattern

--- main.cpp
+++ main.cpp
@@ -14,20 +14,18 @@ int main()
     auto context = Cairo::Context::create(surface);
     context->set_antialias(Cairo::ANTIALIAS_DEFAULT);
     context->set_source_rgba(0.3, 0.2, 0.5, 1.0);
-    context->move_to(0.0, 0.0);
-    context->line_to(50., 50.);
-    const double radius = 30.0;
-    context->arc(50., 50., radius, 0.0, 2. * std::numbers::pi);
-    context->close_path();
-    context->fill();
-    context->set_line_width(1.0);
+    context->set_line_width(2.0);
+    for (int i = 0; i < 20; i++) {
+      context->move_to(0., 10. * static_cast<double>(i));
+      for (int j = 0; j < 10; j++) {
+        context->rel_line_to(10., 10.);
+        context->rel_line_to(10., -10.);
+      }
+    }
     context->stroke();
-    context->set_source_rgba(0.5, 0.2, 0.5, .8);
-    context->move_to(100., 100.);
-    context->rotate(std::numbers::pi);
-    context->rectangle(100., 100., 100., 100.);
+    context->set_source_rgba(0.3, 0.2, 0.5, .5);
+    context->arc(100., 100., 100., 0., 2. * std::numbers::pi);
     context->fill();
-    context->stroke();
     surface->write_to_png("test.png");
   } catch (Cairo::logic_error &e) {
     std::cerr << e.what() << '\n';
